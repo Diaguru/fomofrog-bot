3bb29f3234a836f80209cde91f07b2bf9a9151f9
Diaguru <145407956+Diaguru@users.noreply.github.com>
2025-09-24 11:10:58 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,13 @@
+# server.py
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "FomoFrog Bot is running!"
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
